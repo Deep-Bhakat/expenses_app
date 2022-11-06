c8c0f3421ea889dd90fcf994f5904dfe5139d29b
Deep Bhakat <deepbhakat234@gmail.com>
2022-11-06 18:57:41 +0530

    added chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_app/chart.dart';
 import 'package:expenses_app/new_transaction.dart';
 import 'package:expenses_app/transaction.dart';
 import 'package:expenses_app/transaction_list.dart';
@@ -51,6 +52,16 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  List<Transaction> get _recentTransactions {
+    return transactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,16 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('Chart Area'),
-                color: Colors.amber,
-              ),
-            ),
-            TransactionList(transactions)
-          ],
+          children: [Chart(_recentTransactions), TransactionList(transactions)],
         ),
       ),
       floatingActionButton: FloatingActionButton(
